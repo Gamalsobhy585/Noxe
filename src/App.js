6c92cb393d7e147bd2b8cd62330ce1fc95974e06
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2024-05-27 03:20:47 +0300

    App edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import People from './components/People/People';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Profile from './components/Profile/Profile';
-import Gallery from './components/Gallery/Gallery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.css';
